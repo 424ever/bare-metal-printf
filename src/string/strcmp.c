f34c4e22c28936462d13b39ec63aade9cd1561db
424ever <jonas.brandstoetter@gmx.at>
2023-08-01 17:30:53 +0200

    Fix problem in strcmp

--- src/string/strcmp.c
+++ src/string/strcmp.c
@@ -1,12 +1,14 @@
+#include <stdio.h>
 #include <string.h>
 
 int strcmp(const char *s1, const char *s2)
 {
 	for (; *s1 == *s2 && *s1; ++s1, ++s2)
 		;
+
 	if (*s1 < *s2)
 		return -1;
-	else if (*s2 > *s1)
+	else if (*s2 < *s1)
 		return 1;
 	else
 		return 0;
